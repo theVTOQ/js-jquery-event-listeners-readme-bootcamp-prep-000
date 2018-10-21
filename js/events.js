b267f31878a34747a1dc3821a96625a40301973a
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-21 22:09:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,10 +5,17 @@ function getIt(){
 
 function frameIt(){
   this.addClass("tasty");
+
+
+function pressIt(key){
+  if(key.which == 71){
+    alert('G was pressed');
+  }
 }
 $(document).ready(function(){
 
 // call functions here
   $('p').on('click', getIt);
   $('img').on('load', frameIt);
+  $('form:input').on('keydown', pressIt)
 });
