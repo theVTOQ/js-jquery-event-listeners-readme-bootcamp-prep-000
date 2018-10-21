953d379a6fb0e2c3850b18d538925ea269de865a
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-21 22:03:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,8 +3,12 @@ function getIt(){
   alert("Hey!");
 }
 
+function frameIt(){
+  this.addClass("tasty");
+}
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on('click', getIt);
+  $('img').on('load', frameIt);
 });
