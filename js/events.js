57d06eee21ec928d03f1697bd9a11983e1117a7c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-21 22:29:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt(){
+  alert("Hey!");
+}
+
+function frameIt(){
+  $('img:last').addClass("tasty");
+}
+/*
+function pressIt(key){
+  if(key.which == 71){
+    alert('G was pressed');
+  }
+}*/
+
+function pressIt(){
+  $('input').on('keydown', function(key){
+    if(key.which == 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+
+function submitIt(){
+  alert('Your form is going to be submitted now.')
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  $('p').on('click', getIt);
+  $('img').on('load', frameIt);
+  //$('form:input').on('keydown', pressIt(key));
+  pressIt();
+  $('form').on('submit', submitIt);
 });
