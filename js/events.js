ac7e631446039e0144833102c23933afc2cc3cc2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-21 22:10:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,10 +12,16 @@ function pressIt(key){
     alert('G was pressed');
   }
 }
+
+function submitIt(){
+  alert('Your form is going to be submitted now.')
+}
+
 $(document).ready(function(){
 
 // call functions here
   $('p').on('click', getIt);
   $('img').on('load', frameIt);
   $('form:input').on('keydown', pressIt)
+  $('form').on('submit', submitIt);
 });
